6f72b61e99f8f8e9bcf26a21bef47a160dcdc7a8
RamiroOrtega9558 <9558@itescam.edu.mx>
2025-03-12 00:24:56 -0600

    Add files via upload

---
+++ Ramiro Joel Ortega Acosta 9558/FiguraGeometrica/FiguraGeometrica.java
@@ -0,0 +1,38 @@
+public class FiguraGeometrica {
+    private String nombre;
+    private String color;
+
+    public FiguraGeometrica(String nombre, String color) {
+        setNombre(nombre);
+        setColor(color);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double calcularPerimetro() {
+        return 0;
+    }
+
+    public double calcularArea() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "FiguraGeometrica{" + "nombre='" + nombre + "', color='" + color + "'}";
+    }
+}
