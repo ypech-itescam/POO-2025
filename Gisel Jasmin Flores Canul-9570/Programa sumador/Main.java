5d502fa6b6c42f5ef1e9d54f3c918e5959d1d3aa
Gisel-Flores <9570@itescam.edu.mx>
2025-03-13 12:47:38 -0600

    Add files via upload

---
+++ Gisel Jasmin Flores Canul-9570/Programa sumador/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Sumadora sumadora = new Sumadora();
+        System.out.println(sumadora.suma(1,2));
+        System.out.println(sumadora.suma2('a','b'));
+        System.out.println(sumadora.suma3(2.5,1.5));
+        System.out.println(sumadora.suma4(4.5, 3));
+    }
+}
---
+++ Gisel Jasmin Flores Canul-9570/Programa sumador/Sumadora.java
@@ -0,0 +1,17 @@
+public class Sumadora{
+    public String suma(int i, int j){
+        return "La suma es: " + (i + j);
+    }
+
+    public String suma2(char i, char j){
+        return "La suma es: " + i + j;
+    }
+
+    public String suma3(double i, double j){
+        return "La suma es: " + (i + j);
+    }
+
+    public String suma4(double i, int j){
+        return "La suma es: " + (i + j);
+    }
+}
