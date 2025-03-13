fd4f2f18b798595e7e6486063a762538e7b1ded8
9583MayraLizzethPucPech <9583@itescam.edu.mx>
2025-03-12 20:55:33 -0600

    Add files via upload

---
+++ Mayra_9583/Sumadora.java
@@ -0,0 +1,24 @@
+public class Sumadora{
+    public String suma(int i, int j){
+        return "la suma es:" + (i + j);
+    }
+    public String suma2(char i, char j){
+        return"la suma es:"+ (i + j);
+    }
+    public String suma3(double i, double j){
+        return "la suma es:" + (i + j);
+    }
+    public String suma4( double i, int j){
+        return"la suma es:" + (i + j);
+    }
+  
+    public static void main(String[] args){
+        Sumadora sumadora = new Sumadora();
+        System.out.println(sumadora.suma(480,630));
+        System.out.println(sumadora.suma2('A', 'B'));
+        System.out.println(sumadora.suma3(9.3, 10.8));
+        System.out.println(sumadora.suma4(2.9, 3));
+
+    }
+
+}
