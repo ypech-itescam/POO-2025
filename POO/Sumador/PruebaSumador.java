53f18f4bd3daa483dac7728903519221c4e3d212
CRISTHIANG77 <cgarridocaamal@gmail.com>
2025-03-12 20:17:12 -0600

    Delete POO directory

--- POO/Sumador/PruebaSumador.java
+++
@@ -1,9 +0,0 @@
-public class PruebaSumador {
-    public static void main(String[] args) {
-        Suma suma = new Suma();
-        System.out.println(suma.suma(10, 10));
-        System.out.println(suma.suma2('A','B'));
-        System.out.println(suma.suma3(2.0,3.0));
-        System.out.println(suma.suma4(2.0,5));
-}
-}
